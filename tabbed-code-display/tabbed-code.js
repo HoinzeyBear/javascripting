da58e1046666fbf95a64d9fe9708e62f02659615
dhoyne <darrenhoyne@gmail.com>
2021-04-21 00:04:21 +0100

    Got me code tabby thing working alrite

--- tabbed-code-display/tabbed-code.js
+++ tabbed-code-display/tabbed-code.js
@@ -1,11 +1,34 @@
 var parentWrappers = document.getElementsByClassName("code-block-wrapper")
 console.log(parentWrappers.length)
 for(var i=0; i < parentWrappers.length; i++){
+    console.log("In wrapper " + i)
     var wrapper = parentWrappers[i]
-    var children = wrapper.children
     var headers = wrapper.querySelectorAll(".cb-header")
     var blocks = wrapper.querySelectorAll(".code-block")
 
-    console.log(headers.length)
-    console.log(blocks.length)
+    headers[0].className += " selected"
+    blocks[0].className += " selected"
+
+    var onClick = function(pos, headers, blocks) {
+        return function() {
+            var blockToHide
+            var headerToHide
+            for(var k=0; k < blocks.length; k++){
+                if(String(blocks[k].className).includes("selected")) {
+                    blockToHide = blocks[k]
+                    headerToHide = headers[k]
+                }
+            }
+            if(blockToHide) {
+                blockToHide.className = String(blockToHide.className).replace(" selected","")
+                blocks[pos].className += " selected"
+
+                headerToHide.className = String(headerToHide.className).replace(" selected","")
+                headers[pos].className += " selected"
+            }
+        }
+    }
+    for(var t=0; t < headers.length; t++) {
+        headers[t].addEventListener("click", onClick(t, headers, blocks))
+    }
 }
